0cba688dad28882a1cb42d3a717792cf90004c7c
Didier Delhaisse <did35@yahoo.com>
2025-10-14 18:43:11 +0200

    Initial Commit

---
+++ DeepCircle/DeepCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepCircleApp.swift
+//  DeepCircle
+//
+//  Created by Didier Delhaisse on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeepCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
